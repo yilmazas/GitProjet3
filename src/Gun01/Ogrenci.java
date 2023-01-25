9962be47c28abf943a7022fbc9e7922e16a9aa76
yilma <yilmazahmetserdar@gmail.com>
2023-01-25 19:50:51 +0100

    ikinci gorevimi yaptim

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -2,7 +2,9 @@ package Gun01;
 
 public class Ogrenci {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+        }
 
     }
 }
